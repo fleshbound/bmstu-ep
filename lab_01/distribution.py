5efc987c65396e5406a414bd0abe5b946eb42804
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2025-02-17 20:44:20 +0300

    fix exponential distribution

--- lab_01/distribution.py
+++ lab_01/distribution.py
@@ -1,4 +1,6 @@
 import abc
+import math
+from random import random
 
 from numpy.random import exponential
 
@@ -9,11 +11,41 @@ class Distribution(abc.ABC):
         return NotImplementedError
 
 
-class ExponentialDistribution(Distribution):
+class PoissonDistribution(Distribution):
+    """
+    Класс, представляющий распределение Пуассона.
+
+    Attributes:
+        lambd (float): Параметр lambda (среднее значение) распределения.
+
+    Methods:
+        generate(): Генерирует случайное значение Пуассоновской случайной величины.
+    """
     lambda_: float
 
-    def __init__(self, lambda_: float):
+    def __init__(self, lambda_):
+        """
+        Конструктор класса PoissonDistribution.
+
+        Args:
+            lambd (float): Параметр lambda (среднее значение) распределения.
+        """
+        if lambda_ <= 0:
+            raise ValueError("Lambda must be a positive value.")
         self.lambda_ = lambda_
 
     def generate(self):
-        return exponential(self.lambda_)
+        """
+        Генерирует случайное значение Пуассоновской случайной величины.
+        Использует алгоритм, основанный на равномерном распределении.
+
+        Returns:
+            int: Случайное значение Пуассоновской случайной величины.
+        """
+        L = math.exp(-self.lambda_)
+        k = 0
+        p = 1
+        while p > L:
+            k += 1
+            p *= random()
+        return k - 1
